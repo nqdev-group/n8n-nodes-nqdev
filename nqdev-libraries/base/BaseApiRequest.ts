c6246118d7e2df818825411863319ca902f4b4e3
nguyenquy0710 <nguyenquy.1096@gmail.com>
2025-05-06 10:11:30 +0700

    QUYIT-149 | Add BaseApiRequest and related modules for API integration

---
+++ nqdev-libraries/base/BaseApiRequest.ts
@@ -0,0 +1,41 @@
+import type {
+  IDataObject,
+  IExecuteFunctions,
+  IHookFunctions,
+  IHttpRequestMethods,
+} from 'n8n-workflow';
+import { nqdevApiRequest } from "../common";
+
+export const NAME_CREDENTIAL = 'nqdevBaseApi';
+
+export const HTTP_HEADERS: IDataObject = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+};
+
+export async function baseApiRequest(
+  this: IHookFunctions | IExecuteFunctions,
+  method: IHttpRequestMethods,
+  endpoint: string,
+  body: IDataObject = {},
+  qs: IDataObject = {},
+  headers: IDataObject = {},
+): Promise<any> {
+  // Lấy credentials từ node
+  const credentials = await this.getCredentials(NAME_CREDENTIAL),
+    baseUrl: string = (credentials?.esmsDomain ?? 'https://rest.esms.vn') as string,
+    esmsApiKey: string = (credentials?.esmsApiKey ?? '') as string,
+    esmsSecretKey: string = (credentials?.esmsSecretKey ?? '') as string;
+
+  const { ApiKey, SecretKey, ...safeBody } = body;
+
+  return await nqdevApiRequest.call(this, NAME_CREDENTIAL, method, baseUrl, endpoint, {
+    ApiKey: esmsApiKey,
+    SecretKey: esmsSecretKey,
+    ...safeBody,
+  }, {
+    ...qs
+  }, {
+    ...headers
+  });
+}
